3b450bafb0fa95b413a3b101651034e9e8360b5c
Mengfei Ren <mengfei.ren@mavs.uta.edu>
2021-05-17 20:31:55 -0500

    Reconstruct repository

---
+++ build/zstack/ZStackMake.bat
@@ -0,0 +1,73 @@
+"C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.3\arm\bin\iccarm.exe" "C:\Users\zfuzzer\zfuzzer\Z-Fuzzer\zstack_iar\Components\stack\bdb\bdb_Reporting.c" -o "C:\Users\zfuzzer\zfuzzer\Z-Fuzzer\zstack_iar\Debug\Tmp" -f "C:\Users\zfuzzer\zfuzzer\Z-Fuzzer\build\zstack\compile_config.xcl"
+
+"C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.3\arm\bin\iccarm.exe" "C:\Users\zfuzzer\zfuzzer\Z-Fuzzer\zstack_iar\components\stack\af\AF.c" -o "C:\Users\zfuzzer\zfuzzer\Z-Fuzzer\zstack_iar\Debug\Tmp" -f "C:\Users\zfuzzer\zfuzzer\Z-Fuzzer\build\zstack\compile_config.xcl"
+
+"C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.3\arm\bin\iccarm.exe" "C:\Users\zfuzzer\zfuzzer\Z-Fuzzer\zstack_iar\Components\stack\bdb\bdb_FindingAndBinding.c" -o "C:\Users\zfuzzer\zfuzzer\Z-Fuzzer\zstack_iar\Debug\Tmp" -f "C:\Users\zfuzzer\zfuzzer\Z-Fuzzer\build\zstack\compile_config.xcl" 
+
+"C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.3\arm\bin\iccarm.exe" "C:\Users\zfuzzer\zfuzzer\Z-Fuzzer\zstack_iar\Components\stack\bdb\bdb.c" -o "C:\Users\zfuzzer\zfuzzer\Z-Fuzzer\zstack_iar\Debug\Tmp" -f "C:\Users\zfuzzer\zfuzzer\Z-Fuzzer\build\zstack\compile_config.xcl"
+
+"C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.3\arm\bin\iccarm.exe" "C:\Users\zfuzzer\zfuzzer\Z-Fuzzer\zstack_iar\Components\stack\bdb\bdb_tlCommissioning.c" -o "C:\Users\zfuzzer\zfuzzer\Z-Fuzzer\zstack_iar\Debug\Tmp" -f "C:\Users\zfuzzer\zfuzzer\Z-Fuzzer\build\zstack\compile_config.xcl" 
+
+"C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.3\arm\bin\iccarm.exe" "C:\Users\zfuzzer\zfuzzer\Z-Fuzzer\zstack_iar\Components\stack\bdb\bdb_touchlink.c" -o "C:\Users\zfuzzer\zfuzzer\Z-Fuzzer\zstack_iar\Debug\Tmp" -f "C:\Users\zfuzzer\zfuzzer\Z-Fuzzer\build\zstack\compile_config.xcl" 
+
+"C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.3\arm\bin\iccarm.exe" "C:\Users\zfuzzer\zfuzzer\Z-Fuzzer\zstack_iar\Components\stack\bdb\bdb_touchlink_initiator.c" -o "C:\Users\zfuzzer\zfuzzer\Z-Fuzzer\zstack_iar\Debug\Tmp" -f "C:\Users\zfuzzer\zfuzzer\Z-Fuzzer\build\zstack\compile_config.xcl" 
+
+"C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.3\arm\bin\iccarm.exe" "C:\Users\zfuzzer\zfuzzer\Z-Fuzzer\zstack_iar\Components\stack\bdb\bdb_touchlink_target.c" -o "C:\Users\zfuzzer\zfuzzer\Z-Fuzzer\zstack_iar\Debug\Tmp" -f "C:\Users\zfuzzer\zfuzzer\Z-Fuzzer\build\zstack\compile_config.xcl" 
+
+"C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.3\arm\bin\iccarm.exe" "C:\Users\zfuzzer\zfuzzer\Z-Fuzzer\zstack_iar\Components\hal\common\hal_assert.c" -o "C:\Users\zfuzzer\zfuzzer\Z-Fuzzer\zstack_iar\Debug\Tmp" -f "C:\Users\zfuzzer\zfuzzer\Z-Fuzzer\build\zstack\compile_config.xcl"
+
+"C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.3\arm\bin\iccarm.exe" "C:\Users\zfuzzer\zfuzzer\Z-Fuzzer\zstack_iar\Components\hal\common\hal_drivers.c" -o "C:\Users\zfuzzer\zfuzzer\Z-Fuzzer\zstack_iar\Debug\Tmp" -f "C:\Users\zfuzzer\zfuzzer\Z-Fuzzer\build\zstack\compile_config.xcl"
+
+"C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.3\arm\bin\iccarm.exe" "C:\Users\zfuzzer\zfuzzer\Z-Fuzzer\zstack_iar\Components\hal\target\CC2538\hal_adc.c" -o "C:\Users\zfuzzer\zfuzzer\Z-Fuzzer\zstack_iar\Debug\Tmp" -f "C:\Users\zfuzzer\zfuzzer\Z-Fuzzer\build\zstack\compile_config.xcl"
+
+"C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.3\arm\bin\iccarm.exe" "C:\Users\zfuzzer\zfuzzer\Z-Fuzzer\zstack_iar\Components\hal\target\CC2538\hal_aes.c" -o "C:\Users\zfuzzer\zfuzzer\Z-Fuzzer\zstack_iar\Debug\Tmp" -f "C:\Users\zfuzzer\zfuzzer\Z-Fuzzer\build\zstack\compile_config.xcl"
+
+"C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.3\arm\bin\iccarm.exe" "C:\Users\zfuzzer\zfuzzer\Z-Fuzzer\zstack_iar\Components\hal\target\CC2538\hal_ccm.c" -o "C:\Users\zfuzzer\zfuzzer\Z-Fuzzer\zstack_iar\Debug\Tmp" -f "C:\Users\zfuzzer\zfuzzer\Z-Fuzzer\build\zstack\compile_config.xcl"
+
+"C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.3\arm\bin\iccarm.exe" "C:\Users\zfuzzer\zfuzzer\Z-Fuzzer\zstack_iar\Components\hal\target\CC2538\hal_key.c" -o "C:\Users\zfuzzer\zfuzzer\Z-Fuzzer\zstack_iar\Debug\Tmp" -f "C:\Users\zfuzzer\zfuzzer\Z-Fuzzer\build\zstack\compile_config.xcl"
+
+"C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.3\arm\bin\iccarm.exe" "C:\Users\zfuzzer\zfuzzer\Z-Fuzzer\zstack_iar\Components\hal\target\CC2538\hal_lcd.c" -o "C:\Users\zfuzzer\zfuzzer\Z-Fuzzer\zstack_iar\Debug\Tmp" -f "C:\Users\zfuzzer\zfuzzer\Z-Fuzzer\build\zstack\compile_config.xcl"
+
+"C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.3\arm\bin\iccarm.exe" "C:\Users\zfuzzer\zfuzzer\Z-Fuzzer\zstack_iar\Components\hal\target\CC2538\hal_mac_cfg.c" -o "C:\Users\zfuzzer\zfuzzer\Z-Fuzzer\zstack_iar\Debug\Tmp" -f "C:\Users\zfuzzer\zfuzzer\Z-Fuzzer\build\zstack\compile_config.xcl"
+
+"C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.3\arm\bin\iccarm.exe" "C:\Users\zfuzzer\zfuzzer\Z-Fuzzer\zstack_iar\Components\hal\target\CC2538\hal_led.c" -o "C:\Users\zfuzzer\zfuzzer\Z-Fuzzer\zstack_iar\Debug\Tmp" -f "C:\Users\zfuzzer\zfuzzer\Z-Fuzzer\build\zstack\compile_config.xcl"
+
+"C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.3\arm\bin\iccarm.exe" "C:\Users\zfuzzer\zfuzzer\Z-Fuzzer\zstack_iar\Components\hal\target\CC2538\hal_sys_ctrl.c" -o "C:\Users\zfuzzer\zfuzzer\Z-Fuzzer\zstack_iar\Debug\Tmp" -f "C:\Users\zfuzzer\zfuzzer\Z-Fuzzer\build\zstack\compile_config.xcl"
+
+"C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.3\arm\bin\iccarm.exe" "C:\Users\zfuzzer\zfuzzer\Z-Fuzzer\zstack_iar\Components\hal\target\CC2538\hal_timer.c" -o "C:\Users\zfuzzer\zfuzzer\Z-Fuzzer\zstack_iar\Debug\Tmp" -f "C:\Users\zfuzzer\zfuzzer\Z-Fuzzer\build\zstack\compile_config.xcl"
+
+"C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.3\arm\bin\iccarm.exe" "C:\Users\zfuzzer\zfuzzer\Z-Fuzzer\zstack_iar\Components\hal\target\CC2538\hal_uart.c" -o "C:\Users\zfuzzer\zfuzzer\Z-Fuzzer\zstack_iar\Debug\Tmp" -f "C:\Users\zfuzzer\zfuzzer\Z-Fuzzer\build\zstack\compile_config.xcl"
+
+"C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.3\arm\bin\iccarm.exe" "C:\Users\zfuzzer\zfuzzer\Z-Fuzzer\zstack_iar\ZMain\TI2538DB\OnBoard.c" -o "C:\Users\zfuzzer\zfuzzer\Z-Fuzzer\zstack_iar\Debug\Tmp" -f "C:\Users\zfuzzer\zfuzzer\Z-Fuzzer\build\zstack\compile_config.xcl"
+
+"C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.3\arm\bin\iccarm.exe" "C:\Users\zfuzzer\zfuzzer\Z-Fuzzer\zstack_iar\Components\osal\common\OSAL.c" -o "C:\Users\zfuzzer\zfuzzer\Z-Fuzzer\zstack_iar\Debug\Tmp" -f "C:\Users\zfuzzer\zfuzzer\Z-Fuzzer\build\zstack\compile_config.xcl"
+
+"C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.3\arm\bin\iccarm.exe" "C:\Users\zfuzzer\zfuzzer\Z-Fuzzer\zstack_iar\Components\osal\common\OSAL_Memory.c" -o "C:\Users\zfuzzer\zfuzzer\Z-Fuzzer\zstack_iar\Debug\Tmp" -f "C:\Users\zfuzzer\zfuzzer\Z-Fuzzer\build\zstack\compile_config.xcl"
+
+"C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.3\arm\bin\iccarm.exe" "C:\Users\zfuzzer\zfuzzer\Z-Fuzzer\zstack_iar\Components\osal\common\OSAL_Timers.c" -o "C:\Users\zfuzzer\zfuzzer\Z-Fuzzer\zstack_iar\Debug\Tmp" -f "C:\Users\zfuzzer\zfuzzer\Z-Fuzzer\build\zstack\compile_config.xcl"
+
+"C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.3\arm\bin\iccarm.exe" "C:\Users\zfuzzer\zfuzzer\Z-Fuzzer\zstack_iar\Components\services\saddr\saddr.c" -o "C:\Users\zfuzzer\zfuzzer\Z-Fuzzer\zstack_iar\Debug\Tmp" -f "C:\Users\zfuzzer\zfuzzer\Z-Fuzzer\build\zstack\compile_config.xcl"
+
+"C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.3\arm\bin\iccarm.exe" "C:\Users\zfuzzer\zfuzzer\Z-Fuzzer\zstack_iar\Source\zcl_genericapp_data.c" -o "C:\Users\zfuzzer\zfuzzer\Z-Fuzzer\zstack_iar\Debug\Tmp" -f "C:\Users\zfuzzer\zfuzzer\Z-Fuzzer\build\zstack\compile_config.xcl"
+
+"C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.3\arm\bin\iccarm.exe" "C:\Users\zfuzzer\zfuzzer\Z-Fuzzer\zstack_iar\Source\zcl_ha.c" -o "C:\Users\zfuzzer\zfuzzer\Z-Fuzzer\zstack_iar\Debug\Tmp" -f "C:\Users\zfuzzer\zfuzzer\Z-Fuzzer\build\zstack\compile_config.xcl"
+
+"C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.3\arm\bin\iccarm.exe" "C:\Users\zfuzzer\zfuzzer\Z-Fuzzer\zstack_iar\Components\stack\zcl\zcl_general.c" -o "C:\Users\zfuzzer\zfuzzer\Z-Fuzzer\zstack_iar\Debug\Tmp" -f "C:\Users\zfuzzer\zfuzzer\Z-Fuzzer\build\zstack\compile_config.xcl"
+
+"C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.3\arm\bin\iccarm.exe" "C:\Users\zfuzzer\zfuzzer\Z-Fuzzer\zstack_iar\Components\stack\zcl\zcl.c" -o "C:\Users\zfuzzer\zfuzzer\Z-Fuzzer\zstack_iar\Debug\Tmp" -f "C:\Users\zfuzzer\zfuzzer\Z-Fuzzer\build\zstack\compile_config.xcl"
+
+"C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.3\arm\bin\iccarm.exe" "C:\Users\zfuzzer\zfuzzer\Z-Fuzzer\zstack_iar\ZMain\ZMain.c"  -o "C:\Users\zfuzzer\zfuzzer\Z-Fuzzer\zstack_iar\Debug\Tmp" -f "C:\Users\zfuzzer\zfuzzer\Z-Fuzzer\build\zstack\compile_config.xcl"
+
+"C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.3\arm\bin\iccarm.exe" "C:\Users\zfuzzer\zfuzzer\Z-Fuzzer\zstack_iar\Components\stack\zdo\ZDObject.c" -o "C:\Users\zfuzzer\zfuzzer\Z-Fuzzer\zstack_iar\Debug\Tmp" -f "C:\Users\zfuzzer\zfuzzer\Z-Fuzzer\build\zstack\compile_config.xcl"
+
+"C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.3\arm\bin\iccarm.exe" "C:\Users\zfuzzer\zfuzzer\Z-Fuzzer\zstack_iar\Components\stack\zdo\ZDConfig.c" -o "C:\Users\zfuzzer\zfuzzer\Z-Fuzzer\zstack_iar\Debug\Tmp" -f "C:\Users\zfuzzer\zfuzzer\Z-Fuzzer\build\zstack\compile_config.xcl"
+
+"C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.3\arm\bin\iccarm.exe" "C:\Users\zfuzzer\zfuzzer\Z-Fuzzer\zstack_iar\Components\stack\zdo\ZDNwkMgr.c" -o "C:\Users\zfuzzer\zfuzzer\Z-Fuzzer\zstack_iar\Debug\Tmp" -f "C:\Users\zfuzzer\zfuzzer\Z-Fuzzer\build\zstack\compile_config.xcl"
+
+"C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.3\arm\bin\iccarm.exe" "C:\Users\zfuzzer\zfuzzer\Z-Fuzzer\zstack_iar\Components\stack\zdo\ZDProfile.c" -o "C:\Users\zfuzzer\zfuzzer\Z-Fuzzer\zstack_iar\Debug\Tmp" -f "C:\Users\zfuzzer\zfuzzer\Z-Fuzzer\build\zstack\compile_config.xcl"
+
+"C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.3\arm\bin\iccarm.exe" "C:\Users\zfuzzer\zfuzzer\Z-Fuzzer\zstack_iar\Components\stack\zdo\ZDSecMgr.c" -o "C:\Users\zfuzzer\zfuzzer\Z-Fuzzer\zstack_iar\Debug\Tmp" -f "C:\Users\zfuzzer\zfuzzer\Z-Fuzzer\build\zstack\compile_config.xcl"
+
+"C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.3\arm\bin\iccarm.exe" "C:\Users\zfuzzer\zfuzzer\Z-Fuzzer\zstack_iar\components\stack\zdo\ZDApp.c" -o "C:\Users\zfuzzer\zfuzzer\Z-Fuzzer\zstack_iar\Debug\Tmp" -f "C:\Users\zfuzzer\zfuzzer\Z-Fuzzer\build\zstack\compile_config.xcl"
+
+"C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.3\arm\bin\ilinkarm.exe" -f "C:\Users\zfuzzer\zfuzzer\Z-Fuzzer\build\zstack\link_config.xcl" -o "C:\Users\zfuzzer\zfuzzer\Z-Fuzzer\zstack_iar\Debug\Exe\zstack.out"
